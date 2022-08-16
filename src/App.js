1c267bc37da1f76dac2a2bf4846b3543c0172fd4
Alfred Rezk <73665419+AlfredRezk@users.noreply.github.com>
2022-08-15 20:48:01 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import MuiButton from "./components/buttons/MuiButton";
+import MuiButtonGroup from "./components/buttons/MuiButtonGroup";
 import MuiTypography from "./components/data-display/MuiTypography";
 
 function App() {
 	return (
 		<div className="App">
-    <MuiButton/>
+      <MuiButtonGroup/>
 		</div>
 	);
 }
